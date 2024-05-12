683ba9e4dcb026bdb5b44481a48e226c76161383
uwihanganye obed <uwihanganyeobed@gmail.com>
2024-05-12 22:31:55 +0200

    set

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -3,6 +3,7 @@
 import { InputHTMLAttributes, forwardRef } from 'react'
 import cn from 'classnames'
 
+
 export const Input = forwardRef<
   HTMLInputElement,
   InputHTMLAttributes<HTMLInputElement>
